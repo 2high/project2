6cbf81d0f1c9b0e1c49dddca0b6df5ceac5196f4
Shauan Ferreira Leite <shauanf@gmail.com>
2017-01-14 15:41:37 -0500

    Adding JSX  to SHARE component

--- coffee-cake/src/components/Share.js
+++ coffee-cake/src/components/Share.js
@@ -13,6 +13,33 @@ const Share = () => (
           <option value="queens">Queens</option>
         </select>
       </div>
+      <div className="col-md-12">
+          <div className="aa">Name of the place</div>
+          <input type="text" name="FirstName" value="Mickey" />
+      </div>
+      <div className="col-md-12">
+        <div className="aa">Coffee ranking (between 1 and 5):</div>
+        <select>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option >5</option>
+        </select>
+      </div>
+      <div className="col-md-12">
+        <div className="aa">Cake ranking (between 1 and 5):</div>
+        <select>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option >5</option>
+        </select>
+      </div>
+      <div className="col-md-12">
+        <button>Share</button>
+      </div>
     </div>
   </div>
 )
