b79b61ca13b2874eef40bd4f26146ed98d55c0f2
Shauan Ferreira Leite <shauanf@gmail.com>
2017-01-16 21:14:26 -0500

    Style share component

--- coffee-cake/src/components/Share.js
+++ coffee-cake/src/components/Share.js
@@ -33,42 +33,66 @@ class Share extends React.Component{
 
     render() {
       return (
-        <div>
-          <div className="container">
-          <input
-            type="text"
-            ref={(input) => this.location = input}
-            placeholder="Location"
-            />
-          <input
-            type="text"
-            ref={(input) => this.name = input}
-            onKeyPress={(e) => this.keyPress(e)}
-            placeholder="name" />
-          <input
-            type="text"
-            ref={(input) => this.coffee = input}
-            onKeyPress={(e) => this.keyPress(e)}
-            placeholder="coffee ranking" />
-          <input
-            type="text"
-            ref={(input) => this.cake = input}
-            onKeyPress={(e) => this.keyPress(e)}
-            placeholder="cake ranking" />
-          <input
-            type="text"
-            ref={(input) => this.comment = input}
-            onKeyPress={(e) => this.keyPress(e)}
-            placeholder="Comment " />
-          <button
-            id="button"
-            type="submit"
-            onClick={() => this.createLocation()}
-            className="xxx">
-            Submit
-          </button>
-        </div>
-        </div>
+<div>
+  <div className="container">
+    <div className="row">
+      <div className="col-md-8 col-md-offset-2">
+      <div className="form-group sharez">
+        <label>Location</label>
+        <input
+          className="form-control"
+          type="text"
+          ref={(input) => this.location = input}
+          placeholder="Location"
+          />
+      </div>
+      <div className="form-group">
+        <label>Name of the venue</label>
+        <input
+          className="form-control"
+          type="text"
+          ref={(input) => this.name = input}
+          onKeyPress={(e) => this.keyPress(e)}
+          placeholder="name" />
+      </div>
+      <div className="form-group">
+        <label>Coffee score</label>
+        <input
+          className="form-control"
+          type="text"
+          ref={(input) => this.coffee = input}
+          onKeyPress={(e) => this.keyPress(e)}
+          placeholder="coffee ranking" />
+      </div>
+      <div className="form-group">
+        <label>Cake score</label>
+        <input
+          className="form-control"
+          type="text"
+          ref={(input) => this.cake = input}
+          onKeyPress={(e) => this.keyPress(e)}
+          placeholder="cake ranking" />
+      </div>
+      <div className="form-group">
+        <label>Cake score</label>
+        <input
+          className="form-control"
+          type="text"
+          ref={(input) => this.comment = input}
+          onKeyPress={(e) => this.keyPress(e)}
+          placeholder="Comment " />
+      </div>
+        <button
+          id="button"
+          type="submit"
+          onClick={() => this.createLocation()}
+          className="xxx">
+          Submit
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
       )
     }
 
